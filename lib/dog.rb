cb5b9abc14bfdb98509f4b76dde41170dc76c68a
David Cantoran <davidcantoran36@gmail.com>
2019-12-12 02:12:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,16 +11,15 @@ class Dog
   
   def save 
     @@all << self 
-    @@all
   end 
   
   def Dog.all 
     @@all 
   end 
   
-  # def Dog.clear_all 
-  #   @@all.delete 
-  # end 
+  def Dog.clear_all 
+    @@all.delete 
+  end 
   
   def Dog.print_all
     @@all.each do |name| 
